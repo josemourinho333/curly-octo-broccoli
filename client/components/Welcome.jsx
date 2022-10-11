19a8ec6e3caf7842d8bc737d8af8f5f2501863ef
root <root@PhilYoo.localdomain>
2022-10-10 18:28:03 -0700

    Fix bugs and error, successful transaction

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -13,7 +13,7 @@ const Input = ({placeHolder, name, type, value, handleChange}) => {
       type={type}
       step="0.0001"
       value={value}
-      onChange={(e) => handleChange(e)}
+      onChange={(e) => handleChange(e, name)}
       className="my-2 w-full rounded-sm p-2 outline-none bg-white/10 border-none text-sm"
     />
   )
@@ -21,10 +21,17 @@ const Input = ({placeHolder, name, type, value, handleChange}) => {
 
 
 const Welcome = () => {
-  const { connectWallet } = useContext(TransactionContext);
+  const { connectWallet, currentAccount, formData, sendTransaction, handleFormChange } = useContext(TransactionContext);
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { addressTo, amount, keyword, message } = formData;
 
+    if (!addressTo || !amount || !keyword || !message) {
+      return;
+    } else {
+      sendTransaction();
+    }
   };
 
   return (
@@ -37,20 +44,25 @@ const Welcome = () => {
           <p className="text-left mt-5 font-light md:w-9/12 w-11/12 text-base">
             Super minimal blockchain app.
           </p>
-          <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
-            <p className="text-white text-base font-semibold">Connect Your Wallet</p>
-          </button>
+          {!currentAccount 
+            ? <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                <p className="text-white text-base font-semibold">Connect Your Wallet</p>
+              </button>
+            : <button disabled type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-not-allowed hover:bg-[#2546bd]">
+                <p className="text-white text-base font-semibold">Wallet is connected</p>
+              </button>
+          }
         </div>
 
         <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center bg-black text-white rounded-xl">
-            <Input placeHolder="Address To" name="addressTo" type="text" handleChange={() => {}} />
-            <Input placeHolder="ETH Amount" name="amount" type="number" handleChange={() => {}} />
-            <Input placeHolder="Gif (Keyword)" name="keyword" type="text" handleChange={() => {}} />
-            <Input placeHolder="Message" name="message" type="text" handleChange={() => {}} />
+            <Input placeHolder="Address To" name="addressTo" type="text" handleChange={handleFormChange} />
+            <Input placeHolder="ETH Amount" name="amount" type="number" handleChange={handleFormChange} />
+            <Input placeHolder="Gif (Keyword)" name="keyword" type="text" handleChange={handleFormChange} />
+            <Input placeHolder="Message" name="message" type="text" handleChange={handleFormChange} />
             <div className="h-[1px] w-full bg-gray-400 my-2"/>
 
-            {true ? (
+            {false ? (
               <Loader />
             ) : (
               <button type="button" onClick={handleSubmit} className="text-white bg-[#2952e3] w-full mt-2 border-[1px] p-2 border-[#2952e3] rounded-full cursor-pointer">
--- client/context/TransactionContext.jsx
+++ client/context/TransactionContext.jsx
@@ -11,22 +11,44 @@ const getEthereumContract = () => {
   const signer = provider.getSigner();
   const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-  console.log({
-    provider,
-    signer,
-    transactionContract
-  })
+  return transactionContract;
 }
 
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [formData, setFormData] = useState({
+    addressTo: '',
+    amount: '',
+    keyword: '',
+    message: ''
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [transactionCount, setTransactionCount] = useState(typeof window !== 'undefined'&& localStorage.getItem('transactionCount'));
+
+  const handleFormChange = (e, name) => {
+    setFormData(prev => ({
+      ...prev,
+      [name]: e.target.value
+    }));
+  };
 
   const checkIfWalletIsConnected = async () => {
-    if (!ethereum) return alert("You need to install MetaMask");
+    try {
+      if (!ethereum) return alert("You need to install MetaMask");
 
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+  
+      if (accounts.length) {
+        setCurrentAccount(accounts[0]);
+        // getalltransactions per walle 
+      } else {
+        console.log('no account found')
+      }
+    } catch (error) {
+      console.log(error);
 
-    console.log(accounts);
+      throw new Error("No ethereum object");
+    }
   };
 
   const connectWallet = async() => {
@@ -35,19 +57,58 @@ export const TransactionProvider = ({ children }) => {
 
       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
       setCurrentAccount(accounts[0]);
-    } catch {
+    } catch (error) {
       console.log(error);
 
       throw new Error("No ethereum object");
     }
-  }
+  };
+
+  const sendTransaction = async() => {
+
+    try {
+      if (!ethereum) return alert("You need to install MetaMask");
+
+      const { addressTo, amount, keyword, message } = formData;
+      const transactionContract = getEthereumContract();
+      const amountParsed = ethers.utils.parseEther(amount);
+
+      await ethereum.request({
+        method: "eth_sendTransaction",
+        params: [{
+          from: currentAccount,
+          to: addressTo,
+          gas: '0x5208', // 21000 gwei 
+          value: amountParsed._hex, // amount converted to gwei via parseEther
+        }]
+      });
+
+      const transactionHash = await transactionContract.addToBlockchain(addressTo, amountParsed, message, keyword);
+
+      setIsLoading(true);
+      console.log(`Loading - ${transactionHash.hash}`);
+      await transactionHash.wait();
+
+      setIsLoading(false);
+      console.log(`Success - ${transactionHash.hash}`);
+
+      const transactionCount = await transactionContract.getTransactionCount();
+
+      setTransactionCount(transactionCount.toNumber());
+
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No ethereum object");
+    }
+  };
 
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
 
   return (
-    <TransactionContext.Provider value={{ connectWallet }}>
+    <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleFormChange, sendTransaction }}>
       {children}
     </TransactionContext.Provider>
   )
